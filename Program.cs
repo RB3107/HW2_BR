02753a25e41235a1c06dac10bd6e8c0b6151f0e6
Raisa01~ <zarvirova@mail.ru>
2022-11-26 20:02:01 +0300

    Task 13

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-bool day_of_the_week(int day)
+/*bool day_of_the_week(int day)
 {
 if(day >= 6 && day <= 7)
 {
@@ -27,4 +27,45 @@ Console.WriteLine($"День недели {DayWeek} рабочий");
 else
 {
    Console.WriteLine($"Нет такого дня недели");
+}*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void ThirdDigit(int digit)
+{
+if(digit>=100)
+{
+while(digit>999)
+{
+digit=digit/10;
 }
+digit=digit%10;
+Console.WriteLine($"Third digit of a given number {digit}");
+void ThirdDigit(int digit)
+{
+    if(digit>=100)
+    {
+        while(digit>999)
+        {
+            digit=digit/10;
+        }
+        digit=digit%10;
+        Console.WriteLine($"Third digit of a given number {digit}");
+    }
+    else
+    {
+        Console.WriteLine("There is no third number.");
+    }
+}
+Console.WriteLine($"Enter the number");
+int digit1=Convert.ToInt32(Console.ReadLine());
+ThirdDigit(digit1);
+{digit}");
+}
+else
+{
+Console.WriteLine("There is no third number.");
+}
+}
+Console.WriteLine($"Enter the number");
+int digit1=Convert.ToInt32(Console.ReadLine());
+ThirdDigit(digit1);
