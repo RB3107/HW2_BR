e098641b883db1f8be8b176db4bcb4561a060f51
Raisa01~ <zarvirova@mail.ru>
2022-11-26 20:23:50 +0300

    Tasks 10.13.15

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ int digit1=Convert.ToInt32(Console.ReadLine());
 ThirdDigit(digit1);*/
 
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int digit2(int number)
+/*int digit2(int number)
 {
     int chislo = number%100;
     chislo=chislo/10;
@@ -60,4 +60,4 @@ int digit2(int number)
 Console.WriteLine($"Введите техзначное число");
 int number3=Convert.ToInt32(Console.ReadLine());
 int numeric = digit2(number3);
-Console.WriteLine($"Вторая цифра в числе {number3} это {numeric}");
+Console.WriteLine($"Вторая цифра в числе {number3} это {numeric}");*/
